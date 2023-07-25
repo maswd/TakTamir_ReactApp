1a29fbe7d042a73b169e775d87f2b75245f9d7c6
Masoud Gorgani <alone.3303@gmail.com>
2023-07-26 01:07:43 +0330

    profile

--- src/app/components/techncian/Profile.jsx
+++ src/app/components/techncian/Profile.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 function Profile() {
   return (
     <>
-      <div className=" mt-2 ">
-        <div className=" col-lg-10 mx-auto mb-4 mb-lg-0">
+      <div className="  mt-2 ">
+        <div className=" col-md-10 col-lg-12 mx-auto mb-4 mb-lg-0">
           <div className="card shadow-lg" style={{ direction: "ltr" }}>
             <div className="row">
               <div className="col-12 col-md-4 ">
                 <div
                   className="
-                    text-center text-white "
+                    text-center text-white h-100 relative"
                   style={{
                     borderTopLeftRadius: ".5rem",
                     borderTopRightRadius: ".5rem",
@@ -19,24 +19,36 @@ function Profile() {
                       "linear-gradient(to right bottom, rgba(246, 211, 101, 1), rgba(253, 160, 133, 1))",
                   }}
                 >
+                   <div className="mb-5 d-flex justify-content-start pl-3 pt-3 align-items-center ">
+                    <Link to="/" className="mr-3">
+                      <div className="shadow-sm border btn-circle text-light ">
+                        <i className="fas fa-sign-out-alt "> </i>
+                      </div>
+                    </Link>
+                    <div className="shadow bg-gradient-info btn-circle my-2 ">
+                      <i className=" fas fa-info-circle  "> </i>
+                    </div>
+                  </div>
                   <img
                     src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
                     alt="avatar"
-                    className="img-fluid my-5 "
-                    style={{ width: "80px" }}
+                    className="img-fluid w-50"
+                    
                   />
-                  <p className="h5">اقای هاشمی</p>
-                  <p className="pb-3 ">حساب ذخیره : 2,000,000</p>
+                 
+                  <p className="h5 mt-3">اقای هاشمی</p>
+                  <p className="h6 my-3">حساب ذخیره : 2,000,000</p>
+                 
                 </div>
+                
               </div>
-              <div className="col-12 h-50 col-md-8 w-100">
-                <div className="card-body p-4 w-100">
-                  <div className="mb-3">
-                    <span>5</span>{" "}
+              <div className=" col-12 col-md-8 w-50 ">
+                <div className="order-0 card-body p-4 w-100">
+                  <div className="pb-3 border-bottom">
+                    <span> 5 </span>
                     <span className="text-gray-900">: امتیاز شما (از 10)</span>
                   </div>
-                  <hr className="mt-0 mb-4" />
-                  <h6 className="mb-3 text-gray-900"> : تخصص ها </h6>
+                  <h6 className="py-3  text-gray-900"> : تخصص ها </h6>
                   <div className="row pt-1" style={{ direction: "rtl" }}>
                     <div className="col-4 mb-3">
                       <p className="text-muted">یخچال</p>
@@ -48,25 +60,27 @@ function Profile() {
                       <p className="text-muted">کولر گازی </p>
                     </div>
                   </div>
-                  <hr className="mt-3" />
-                  <h6 className="mt-4">
-                    <span className="text-gray-900">کد تکنسین :</span> 5565465{" "}
-                  </h6>
                 </div>
+                <div className="d-flex border-top order-1 justify-content-around p-3">
+                  <img
+                    src="/img/undraw_profile_re_4a55.svg"
+                    className="w-50 h-50 "
+                    alt=""
+                  />
+                  <div className="text-right align-self-end">
+                    <h5 className="h6 mt-4">
+                      <span className="text-gray-900">کد تکنسین :</span> 5565465{" "}
+                    </h5>
+                  </div>
+                  
+                </div>
+                
               </div>
+              
             </div>
-          </div>
-          <div className=" d-flex flex-column justify-content-center align-items-center">
-            <div className="shadow w-100 btn text-light bg-gradient-warning my-2 ">
-              <span className="text text-nowrap">قوانین </span>
-            </div>
-            <Link to="/" className="w-100">
-              <div className="shadow w-100 btn text-light bg-gradient-danger ">
-                <span className="text text-nowrap">خروج </span>
-              </div>
-            </Link>
           </div>
         </div>
+        
       </div>
     </>
   );
