a7eecbee8eaa9c9242b433fbf33b7bb8b32bb34d
Masoud Gorgani <alone.3303@gmail.com>
2023-08-04 23:22:30 +0330

    overflow landing

--- src/app/pages/Landing.jsx
+++ src/app/pages/Landing.jsx
@@ -1,26 +1,21 @@
-import Features from "../components/landing/Features"
-import Footer from "../components/landing/Footer"
-import Header from "../components/landing/Header"
-import Hero from "../components/landing/Hero"
-import Pricing from "../components/landing/Pricing"
-import Conect from "../components/landing/Connect"
-
+import Features from "../components/landing/Features";
+import Footer from "../components/landing/Footer";
+import Header from "../components/landing/Header";
+import Hero from "../components/landing/Hero";
+import Pricing from "../components/landing/Pricing";
+import Conect from "../components/landing/Connect";
 
 function Landing() {
-  
-    return (
-       
-        <>
-        
-            <Header />
-            <Hero />
-            <Features />
-            <Conect/>
-            <Pricing />
-            <Footer />
-        </>
-    )
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Hero />
+      <Features />
+      <Conect />
+      <Pricing />
+      <Footer />
+    </div>
+  );
 }
 
-
-export default Landing
+export default Landing;
