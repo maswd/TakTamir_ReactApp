7e3907bacab47aaf402627a1d5850f20bd0affbb
Masoud Gorgani <alone.3303@gmail.com>
2023-08-05 03:48:30 +0330

    chat enter

--- src/app/components/chat/ChatAdmin.jsx
+++ src/app/components/chat/ChatAdmin.jsx
@@ -112,21 +112,32 @@ const ChatAdmin = () => {
     },
     [jonAdminroom, setNameRoom]
   );
-
+ 
   const sendMessage = useCallback(
     async (message) => {
       try {
-        const NameRoom = localStorage.getItem("NameRoom") || nameRoom;
-
-        await connection.invoke("SendMessage", message, NameRoom);
         setMessageUser("");
+        const NameRoom = localStorage.getItem("NameRoom") || nameRoom;
+        await connection.invoke("SendMessage", message, NameRoom);
       } catch (e) {
         console.error(e);
       }
     },
     [connection, nameRoom]
   );
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Enter") {
+        sendMessage(messageUser);
+      }
+    };
 
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [sendMessage, messageUser]);
   return (
     <div>
       <div className="card">
@@ -153,93 +164,96 @@ const ChatAdmin = () => {
               ))}
             </span>
           </div>
-          {connection!==undefined && connection?._connectionState === "Connecting" && (
-            <div
-              className="spinner-border text-info mx-auto align-self-center"
-              role="status"
-            ></div>
-          )}
-          {connection!==undefined && connection?._connectionState === "Connected" && connection._connectionStarted && (
-            <div className="col-12 col-lg-7 col-xl-9 border">
-              <div className="py-2 px-4 border-bottom">
-                <div className="d-flex align-items-center py-1">
-                  <div className="position-relative ">
-                    <img
-                      src="https://bootdey.com/img/Content/avatar/avatar3.png"
-                      className="rounded-circle ml-1 "
-                      alt="Sharon Lessman"
-                      width="40"
-                      height="40"
-                    />
-                  </div>
-                  <div className="flex-grow-1 pr-3">
-                    <strong>{localStorage.getItem("NameRoom")}</strong>
-                    <div className="text-muted small">
-                      {/* <em>Typing...</em> */}
+          {connection !== undefined &&
+            connection?._connectionState === "Connecting" && (
+              <div
+                className="spinner-border text-info mx-auto align-self-center"
+                role="status"
+              ></div>
+            )}
+          {connection !== undefined &&
+            connection?._connectionState === "Connected" &&
+            connection._connectionStarted && (
+              <div className="col-12 col-lg-7 col-xl-9 border">
+                <div className="py-2 px-4 border-bottom">
+                  <div className="d-flex align-items-center py-1">
+                    <div className="position-relative ">
+                      <img
+                        src="https://bootdey.com/img/Content/avatar/avatar3.png"
+                        className="rounded-circle ml-1 "
+                        alt="Sharon Lessman"
+                        width="40"
+                        height="40"
+                      />
+                    </div>
+                    <div className="flex-grow-1 pr-3">
+                      <strong>{localStorage.getItem("NameRoom")}</strong>
+                      <div className="text-muted small">
+                        {/* <em>Typing...</em> */}
+                      </div>
+                    </div>
+                    <div>
+                      <button className="btn btn-light border btn-lg px-3">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="feather feather-more-horizontal feather-lg"
+                        >
+                          <circle cx="12" cy="12" r="1"></circle>
+                          <circle cx="19" cy="12" r="1"></circle>
+                          <circle cx="5" cy="12" r="1"></circle>
+                        </svg>
+                      </button>
                     </div>
                   </div>
-                  <div>
-                    <button className="btn btn-light border btn-lg px-3">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="feather feather-more-horizontal feather-lg"
-                      >
-                        <circle cx="12" cy="12" r="1"></circle>
-                        <circle cx="19" cy="12" r="1"></circle>
-                        <circle cx="5" cy="12" r="1"></circle>
-                      </svg>
+                </div>
+                <div className="position-relative">
+                  <div className="chat-messages p-4">
+                    {allmessageUser.length === 0 ? (
+                      <p>
+                        <div class="alert alert-info" role="alert">
+                          پیامی وجود ندارد
+                        </div>
+                      </p>
+                    ) : (
+                      allmessageUser?.map((m, index) =>
+                        m.Sender === "Admin" ? (
+                          <MassgeRight key={index} data={m} />
+                        ) : (
+                          <MassageLeft key={index} data={m} />
+                        )
+                      )
+                    )}
+
+                    <div ref={messagesEndRef} />
+                  </div>
+                </div>
+                <div className="flex-grow-0  py-3 px-4 border-top mb-2">
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="پیام خود را ارسال کنید !"
+                      value={messageUser}
+                      onChange={(e) => setMessageUser(e.target.value)}
+                    />
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => sendMessage(messageUser)}
+                    >
+                      ارسال
                     </button>
                   </div>
                 </div>
               </div>
-              <div className="position-relative">
-                <div className="chat-messages p-4">
-                  {allmessageUser.length === 0 ? (
-                    <p>
-                      <div class="alert alert-info" role="alert">
-                        پیامی وجود ندارد
-                      </div>
-                    </p>
-                  ) : (
-                    allmessageUser?.map((m, index) =>
-                      m.Sender === "Admin" ? (
-                        <MassgeRight key={index} data={m} />
-                      ) : (
-                        <MassageLeft key={index} data={m} />
-                      )
-                    )
-                  )}
-
-                  <div ref={messagesEndRef} />
-                </div>
-              </div>
-              <div className="flex-grow-0  py-3 px-4 border-top mb-2">
-                <div className="input-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="پیام خود را ارسال کنید !"
-                    value={messageUser}
-                    onChange={(e) => setMessageUser(e.target.value)}
-                  />
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => sendMessage(messageUser)}
-                  >
-                    ارسال
-                  </button>
-                </div>
-              </div>
-            </div>
-          )}
+            )}
         </div>
       </div>
     </div>
